d0f3fe71a674066770880dfadfafed5bd3ed2cf8
vini87kk <viniciusmsouzaa00@gmail.com>
2026-05-27 11:25:52 -0300

    versao 1.6

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def cadastro():
     return render_template('cadastro.html')
 
+@app.route('/dados')
+def bancodedados():
+    return render_template('bancodedados.html')
+
 if __name__ == '__main__':
     
     app.run(debug=True , host='0.0.0.0')
